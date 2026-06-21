eb696d4308af620d8f48d4440061065de5445c4f
cvg83unir <carlosdomingo.v1688@comunidadunir.net>
2026-06-21 20:44:19 +0200

    Mejora en enemigos de tipo patrulla, cambian de sentido al colisionar con algo

--- Assets/Systems/AI/Behaviour/Scripts/EnemyPatrol.cs
+++ Assets/Systems/AI/Behaviour/Scripts/EnemyPatrol.cs
@@ -1,20 +1,21 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyPatrol : MonoBehaviour
 {
     private CharacterController2D characterController;
     private Life life;
 
-    //[SerializeField]private Vector2
-
-    //        [SerializeField] private Transform[] patrolPathPoints;
-
-    [SerializeField] private float maxXValue = 5;
-    [SerializeField] private float maxYValue = 5;
+    //[SerializeField] private float maxXValue = 5;
+    //[SerializeField] private float maxYValue = 5;
     [SerializeField] float maxSecondsSameDirection = 2f;
     private float directionSecondsCounter = 0f;
     private Vector2 movementDirection = Vector2.zero;
+    private bool changeDirectionDueToCollision = false;
+    //private bool changeDirectionDueToOutOfBounds = false;
+
 
 
     private void Awake()
@@ -22,6 +23,18 @@ public class EnemyPatrol : MonoBehaviour
         //Cacheamos el componente de character controller y de vida
         this.characterController = GetComponent<CharacterController2D>();
         this.life = GetComponent<Life>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Math.Abs(this.transform.position.x)> this.maxXValue || Math.Abs(this.transform.position.y) > this.maxYValue)
+        //{
+        //    Debug.Log("Límites sobrepasados. X actual: " + this.transform.position.x + ". Y actual: " + this.transform.position.y);
+        //    this.changeDirectionDueToOutOfBounds = true;
+        //}
+
     }
 
     private void OnEnable()
@@ -81,6 +94,8 @@ public class EnemyPatrol : MonoBehaviour
 
         //Reseteamos el contador de segundos de movimeinto en la misma dirección:
         this.directionSecondsCounter = 0;
+        this.changeDirectionDueToCollision = false;
+        //this.changeDirectionDueToOutOfBounds = false;
 
         return searchDirection;
 
@@ -90,17 +105,28 @@ public class EnemyPatrol : MonoBehaviour
     {
         if (this.directionSecondsCounter >= this.maxSecondsSameDirection)
         {
+            Debug.Log("Enemigo, cambiar sentido por máximo número de segundos en la misma dirección");
             return true;
         }
+        else if(this.changeDirectionDueToCollision == true)
+        {
+            Debug.Log("Enemigo, cambiar sentido por colisión");
+            return true;
+        }
+        //else if (this.changeDirectionDueToOutOfBounds == true)
+        //{
+        //    Debug.Log("Enemigo, cambiar sentido por irse fuera de límites establecidos");
+        //    return true;
+        //}
         else
             return false;
     }
 
-
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D elOtro)
     {
-        
+        Debug.Log("Enemigo ha colisionado con:" + elOtro.ToString());
+        this.changeDirectionDueToCollision = true;
     }
+
+
 }
